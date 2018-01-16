03e3c073cf44d6c001bba852aff97bc855f2c5ff
lcoelho <luisfs123@gmail.com>
2018-01-16 21:24:53 -0200

    eslint errors fixed

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,5 @@
-import { expect } from 'chai'
-import {sum, sub, mult, div} from '../src/main'
+import { expect } from 'chai';
+import { sum, sub, mult, div } from '../src/main';
 
 describe('calc', () => {
   describe('smoke tests', () => {
@@ -23,37 +23,36 @@ describe('calc', () => {
       expect(div).to.be.a('function');
     });
   });
-  
+
   describe('Sum', () => {
-  	it('should return 4 when "sum(2, 2)"', () => {
-  	  expect(sum(2, 2)).to.be.equal(4);
-  	});
+    it('should return 4 when "sum(2, 2)"', () => {
+      expect(sum(2, 2)).to.be.equal(4);
+    });
   });
 
   describe('Sub', () => {
-  	it('should return 4 when "sub(6, 2)"', () => {
-  	  expect(sub(6, 2)).to.be.equal(4);
-  	});
+    it('should return 4 when "sub(6, 2)"', () => {
+      expect(sub(6, 2)).to.be.equal(4);
+    });
 
-  	it('should return -4 when "sub(6, 10)"', () => {
-  	  expect(sub(6, 10)).to.be.equal(-4);
-  	});
+    it('should return -4 when "sub(6, 10)"', () => {
+      expect(sub(6, 10)).to.be.equal(-4);
+    });
   });
 
   describe('Div', () => {
-  	it('should return 2 when "div(4, 2)"', () => {
-  	  expect(div(4, 2)).to.be.equal(2);
-  	});
-  	
-  	it('should return "não é posivel dividir por zero!" when division by 0', () => {
-  	  expect(div(4, 0)).to.be.equal("não é posivel dividir por zero!");
-  	});
+    it('should return 2 when "div(4, 2)"', () => {
+      expect(div(4, 2)).to.be.equal(2);
+    });
+
+    it('should return "não é posivel dividir por zero!" when division by 0', () => {
+      expect(div(4, 0)).to.be.equal('não é posivel dividir por zero!');
+    });
   });
 
   describe('Mult', () => {
-  	it('should return 4 when "mult(2, 2)"', () => {
-  	  expect(mult(2, 2)).to.be.equal(4);
-  	});
+    it('should return 4 when "mult(2, 2)"', () => {
+      expect(mult(2, 2)).to.be.equal(4);
+    });
   });
-
 });
